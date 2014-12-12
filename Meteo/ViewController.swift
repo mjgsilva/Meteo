4c462f1d9cb158cefde26e359674eb7b485ca2f0
Mário Silva <mjgs.biz@gmail.com>
2014-12-12 17:09:43 +0000

    Project Skeleton

---
+++ Meteo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meteo
+//
+//  Created by Mário Silva on 12/12/14.
+//  Copyright (c) 2014 Mário Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
