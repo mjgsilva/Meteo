e9dc65f83cb7225656e5fd893bda4db393783b14
Mário Silva <mjgs.biz@gmail.com>
2015-01-17 22:41:40 +0000

    CurrentWeater: precipProbability removed GetIconFileName method added

--- Meteo/CurrentWeather.swift
+++ Meteo/CurrentWeather.swift
@@ -14,16 +14,14 @@ struct CurrentWeather {
     var temperature: Int
     var humidity: Double
     var windSpeed: Double
-    var precipProbability: Double
     var summary: String
     var icon: String
     
-    init(time: Int, temperature: Int, humidity: Double, windSpeed: Double, precipProbability: Double, summary: String, icon: String) {
+    init(time: Int, temperature: Int, humidity: Double, windSpeed: Double, summary: String, icon: String) {
         self.time = time
         self.temperature = temperature
         self.humidity = humidity
         self.windSpeed = windSpeed
-        self.precipProbability = precipProbability
         self.summary = summary
         self.icon = icon
     }
@@ -37,6 +35,33 @@ struct CurrentWeather {
     }
     
     func getIcon() -> UIImage {
-        return UIImage(named: icon)!
+        return UIImage(named: getIconFileName())!
+    }
+    
+    private func getIconFileName() -> String {
+        switch icon {
+            case "clear-day":
+                return "clear-day"
+            case "clear-night":
+                return "clear-night"
+            case "rain":
+                return "rain"
+            case "snow":
+                return "snow"
+            case "sleet":
+                return "sleet"
+            case "wind":
+                return "wind"
+            case "fog":
+                return "fog"
+            case "cloudy":
+                return "cloudy"
+            case "partly-cloudy-day":
+                return "partly-cloudy"
+            case "partly-cloudy-night":
+                return "cloudy-night"
+            default:
+                return "default"
+        }
     }
 }
